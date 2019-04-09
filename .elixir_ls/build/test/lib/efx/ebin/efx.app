85dd2904258074cf836a62b50a329dd3c6e9c3bd
Krzysztof Wende <iraasta@gmail.com>
2019-04-09 17:44:57 +0200

    Working replacing files

--- .elixir_ls/build/test/lib/efx/ebin/efx.app
+++ .elixir_ls/build/test/lib/efx/ebin/efx.app
@@ -1,6 +1,7 @@
 {application,efx,
-             [{applications,[kernel,stdlib,elixir,logger,meck]},
+             [{applications,[kernel,stdlib,elixir,logger]},
               {description,"efx"},
-              {modules,['Elixir.Efx']},
+              {modules,['Elixir.Efx','Elixir.Mix.Tasks.Compile.Efx',
+                        'Elixir.Mix.Tasks.Compile.PostEfx']},
               {registered,[]},
               {vsn,"0.1.0"}]}.
--- lib/efx.ex
+++ lib/efx.ex
@@ -13,6 +13,38 @@ defmodule Efx do
 
   """
   def hello do
-    :world
+    :worldss
+  end
+
+  def replace_effects(ast) do
+    Macro.prewalk(ast, fn ast ->
+      replace_call(ast, {IO, :puts, 1})
+    end)
+  end
+
+  def replace_call(ast = {{:., _, [module, function]}, _, args}, {mod, fun, arity}) do
+    IO.puts("replace call")
+    IO.inspect(ast)
+
+    if fun == function && is_module(module, mod) && length(args) == arity do
+      quote do
+        Efx.eff(unquote(mod), unquote(fun), unquote(args))
+      end
+    else
+      ast
+    end
+  end
+
+  def replace_call(ast, _) do
+    ast
+  end
+
+  def is_module({:__aliases__, _, path}, mod) do
+    Module.concat(path) == mod |> IO.inspect()
+  end
+
+  def eff(mod, fun, args) do
+    IO.puts("Calling #{mod} #{fun} #{inspect(args)}")
+    apply(mod, fun, args)
   end
 end
