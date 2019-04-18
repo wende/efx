1af84074f8b6399008d35f244c2432c6a5938f8f
Krzysztof Wende <iraasta@gmail.com>
2019-04-18 19:00:08 +0200

    Dfault effects

---
+++ example/efx_example/.efx.exs
@@ -0,0 +1,13 @@
+[
+  {File, :read, 1},
+  {File, :write, 2},
+  {File, :open, 1},
+  {IO, :puts, 1},
+  {IO, :puts, 2},
+  {IO, :inspect, 1},
+  {IO, :inspect, 2},
+  {IO, :write, 2},
+  {IO, :read, 1},
+  {IO, :read, 2},
+  {Kernel, :send, 2}
+]
